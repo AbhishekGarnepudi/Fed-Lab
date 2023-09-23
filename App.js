06c4ddae87fb17e8983c12167e71a1669112089f
AbhishekGarnepudi <141471911+AbhishekGarnepudi@users.noreply.github.com>
2023-09-23 08:41:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Counter from './Counter';
+import Textcolor from './Textcolor';
+import Tabularform from './Tabularform';
+
+function App() {
+  return (
+    
+      
+<>
+<Counter/>
+<Textcolor/>
+<Tabularform/>
+</>
+    
+  );
+}
+
+export default App;
